215ff2aa113c6f261062c096364a0a5a0dd46023
Anna Golasowska <annagolasowska@gmail.com>
2018-07-24 23:56:48 +0200

    added responsive nav

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -107,4 +107,21 @@ $(document).ready(function() {
       offset: '50%'
     }
   );
+
+  // mobile nav
+
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-ios-menu')) {
+      icon.addClass('ion-ios-close-circle-outline');
+      icon.removeClass('ion-ios-menu');
+    } else {
+      icon.addClass('ion-ios-menu');
+      icon.removeClass('ion-ios-close-circle-outline');
+    }
+  });
 });
